e5f7c02ff8d5d1b202ad9fd48eaf569bf46ab014
igor <igor_m_87@mail.ru>
2022-11-28 15:04:20 +0300

    get data test added

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from starlette import status
 
 from src.config.settings import Settings
@@ -10,3 +12,14 @@ class TestEndpoints:
         url = self.get_url() + '/getdata'
         resp = await api_client.get(url)
         assert resp.status_code == status.HTTP_200_OK
+
+        channels = resp.json()['channels']
+        assert len(channels) == 4
+        for ch in channels:
+            assert 1 <= ch['num'] <= 4
+            assert isinstance(ch['current'], float)
+            assert isinstance(ch['voltage'], float)
+            try:
+                datetime.fromisoformat(ch['update_dt'])
+            except ValueError:
+                assert False
